9778d7680eba419751ea89d76a8883809642941d
Rowan Clarke <clarker280@gmail.com>
2021-07-13 11:47:48 +0100

    Train Model
    TODO: Learning Rate, Testing

--- src/main.rs
+++ src/main.rs
@@ -7,11 +7,17 @@ use layer::{feed::Feed, parallel::Parallel};
 use model::Model;
 
 fn main() {
-    let mut model = Model::new(5);
-    model.push_layer::<Feed<Sigmoid>>(7);
-    model.push_group::<Parallel>();
-    model.push_layer::<Feed<Sigmoid>>(4);
-    model.push_layer::<Feed<Sigmoid>>(4);
-    model.pop_group();
+    let mut model = Model::new(2);
     model.push_layer::<Feed<Sigmoid>>(2);
+    model.push_layer::<Feed<Sigmoid>>(1);
+
+    let inputs = &[
+        vec![0.0, 0.0],
+        vec![0.0, 1.0],
+        vec![1.0, 0.0],
+        vec![1.0, 1.0],
+    ];
+    let targets = &[vec![0.0], vec![1.0], vec![1.0], vec![0.0]];
+
+    model.train(inputs, targets, 10000, 0.5);
 }
--- src/model.rs
+++ src/model.rs
@@ -1,30 +1,59 @@
 use super::layer::{series::Series, Dynamic, Fixed, Group, Layer, Object};
+use std::mem;
 
 pub struct Model {
     stack: Vec<Box<dyn Group>>,
+    series: Box<Series>,
 }
 
 impl Model {
     pub fn new(size: usize) -> Self {
         Self {
-            stack: vec![Box::new(Series::new(size))],
+            stack: Vec::new(),
+            series: Box::new(Series::new(size)),
+        }
+    }
+
+    fn last(&mut self) -> &mut dyn Group {
+        let len = self.stack.len();
+        if len == 0 {
+            return self.series.as_mut();
+        } else {
+            return self.stack[len].as_mut();
         }
     }
 
     pub fn push_group<G: Group + Dynamic>(&mut self) {
-        let size = self.stack[self.stack.len() - 1].after();
+        // Pushed group must go onto stack
+        let size = self.last().after();
         self.stack.push(Box::new(G::new(size)));
     }
 
     pub fn pop_group(&mut self) {
+        // Popped group must be from stack
         let group = self.stack.pop().unwrap();
-        let last = self.stack.len() - 1;
-        self.stack[last].push(Object::Group(group));
+        self.last().push(Object::Group(group));
     }
 
     pub fn push_layer<L: Layer + Fixed>(&mut self, after: usize) {
-        let last = self.stack.len() - 1;
-        let before = self.stack[last].before();
-        self.stack[last].push(Object::Layer(Box::new(L::new(before, after))));
+        let last = self.last();
+        let before = last.before();
+        last.push(Object::Layer(Box::new(L::new(before, after))));
+    }
+
+    pub fn train(&mut self, inputs: &[Vec<f32>], targets: &[Vec<f32>], epochs: u32, lr: f32) {
+        for e in 0..epochs {
+            for x in 0..inputs.len() {
+                let output = self.series.forward(inputs[x].clone());
+                if (e % 100) == 0 {
+                    let mut cost = 0.0;
+                    for i in 0..self.series.after() {
+                        cost += (output[i] - targets[x][i]).powf(2.0);
+                    }
+                    println!("Epoch: {}, Input: {}, Cost: {}", e, x, cost);
+                }
+                self.series.backward(output);
+            }
+        }
     }
 }
